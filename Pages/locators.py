cf50c9104c7462fab0c297d1817f0ff2093cad9a
Dmitriy <142100741+Minzdraff77@users.noreply.github.com>
2023-11-05 14:03:50 +0400

    Добавил тест добавления в корзину

--- Pages/locators.py
+++ Pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") # css селектор для формы логина
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form") # css селектор для формы регистрации
+    
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "[class*=btn-add-to-basket]")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    CART_MESSAGES_NAME = (By.CSS_SELECTOR, "div[class^=alert]:nth-of-type(1) .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    CART_MESSAGES_PRICE = (By.CSS_SELECTOR, "div[class^=alert]:nth-of-type(3) .alertinner strong")
---
+++ Pages/product_page.py
@@ -0,0 +1,39 @@
+import time
+from .base_page import BasePage
+from .locators import ProductPageLocators as PPL
+
+
+class ProductPage(BasePage):
+    '''
+    Открытие страницы продукта происходит в test_product_page.py.
+    1. Нажимаем на кнопку "Добавить в корзину".
+    2. Вводим проверочный код в алерт и отправляем в консоль проверочный код. Используем метод solve_quiz_and_get_code() из родительского класса.
+    3. Проверяем что есть сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+    4. Проверяем что есть сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара. 
+    '''
+
+    def add_item_to_cart(self): 
+        self.click_button()
+        self.verify_item_added_to_cart()
+        self.verify_total_price()
+        
+    def click_button(self):
+        button = self.browser.find_element(*PPL.ADD_TO_CART_BUTTON)
+        button.click()
+        assert True
+        
+    def enter_verification_code(self):
+        super().solve_quiz_and_get_code()
+        
+    def verify_item_added_to_cart(self): 
+        # выполнить поиск имени продукта в сообщениях корзины
+        product_name = self.browser.find_element(*PPL.PRODUCT_NAME).text 
+        card_messages = self.browser.find_element(*PPL.CART_MESSAGES_NAME).text
+        assert product_name in card_messages, f"Product name {product_name} not found in {card_messages}"
+
+    
+    def verify_total_price(self):
+        # Сообщение со стоимостью корзины совпадает с ценой товара.
+        product_price = self.browser.find_element(*PPL.PRODUCT_PRICE).text
+        card_messages = self.browser.find_element(*PPL.CART_MESSAGES_PRICE).text
+        assert product_price in card_messages, f"Product price {product_price} not found in {card_messages}"
