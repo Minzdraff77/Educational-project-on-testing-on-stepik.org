2fb5084cdf2d2b00a0c81b0c5fdaa5702a315e92
Dmitriy <142100741+Minzdraff77@users.noreply.github.com>
2023-11-07 13:29:51 +0400

    Полировка кода

--- test_product_page.py
+++ test_product_page.py
@@ -22,15 +22,7 @@ def test_guest_can_add_product_to_basket(browser, num_action):
     page = PP(browser, link)        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
     page.add_item_to_cart()         # нажимаем на кнопку "Добавить в корзину"
-
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = PP(browser, link)
-    page.open()
-    page.go_to_login_page()
-
+    
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
@@ -46,7 +38,15 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.should_be_empty_basket()
     page.should_be_basket_message()
+    
 
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PP(browser, link)
+    page.open()
+    page.go_to_login_page()
+    
 
 @pytest.mark.mod1
 class TestUserAddToBasketFromProductPage():
